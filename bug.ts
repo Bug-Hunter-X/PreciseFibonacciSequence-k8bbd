b77bc393d0678378065cc979a0f752448b2119e6
BugHunter-X <askzenai@gmail.com>
2024-12-04 19:56:06 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,11 @@
+function printFibonacci(n: number): void {
+  let a = 0, b = 1, temp;
+  for (let i = 0; i < n; i++) {
+    console.log(a);
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+}
+
+printFibonacci(10); //This will not print correct Fibonacci Sequence for 10 numbers
